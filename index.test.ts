a278ae94eb9c4ce45ca4cda06be33d78f3b8bd11
FranklynKleiton <franklynkleiton18@gmail.com>
2022-10-15 22:42:13 -0300

    Do refactoring.

--- index.test.ts
+++ index.test.ts
@@ -1,20 +1,48 @@
-class MagicFormula {
-  private _companies: any[];
+class Company {
+  private _ticket: string;
+  private _priceEarnings: number;
+  private _returnOnAsset: number;
 
-  constructor(companies: any[]) {
+  constructor(ticket: string, priceEarnings: number, returnOnAsset: number) {
+    this._ticket = ticket;
+    this._priceEarnings = priceEarnings;
+    this._returnOnAsset = returnOnAsset;
+  }
+
+  get ticket() {
+    return this._ticket;
+  }
+
+  get priceEarnings() {
+    return this._priceEarnings;
+  }
+
+  get returnOnAsset() {
+    return this._returnOnAsset;
+  }
+}
+
+class MagicFormula {
+  private _companies: Company[];
+
+  constructor(companies: Company[]) {
     this._companies = companies;
   }
 
   filterPriceEarnings(start: number, end: number) {
-    this._companies = this._companies.filter((c) => {
+    this.filterCompanies((c: Company) => {
       return c.priceEarnings >= start && c.priceEarnings <= end;
     });
   }
 
   filterReturnOnAsset(start: number, end: number) {
-    this._companies = this._companies.filter((c) => {
+    this.filterCompanies((c: Company) => {
       return c.returnOnAsset >= start && c.returnOnAsset <= end;
     });
+  }
+
+  private filterCompanies(expression: (c: Company) => boolean) {
+    this._companies = this._companies.filter((c) => expression(c));
   }
 
   get companies() {
@@ -25,29 +53,30 @@ class MagicFormula {
 describe("magic formula", () => {
   test("should apply price earnings filter", () => {
     const data = [
-      { ticket: "eternit3", priceEarnings: 2.5, returnOnAsset: 20 },
-      { ticket: "tasa3", priceEarnings: 2, returnOnAsset: 20 },
-      { ticket: "tasa3", priceEarnings: 5, returnOnAsset: 20 },
+      new Company("eternit3", 2.5, 20),
+      new Company("eternit3", 2, 20),
+      new Company("eternit3", 5, 20),
     ];
     const formula = new MagicFormula(data);
 
     formula.filterPriceEarnings(2.5, 4);
 
-    expect(formula.companies).toHaveLength(1);
-    expect(formula.companies[0].priceEarnings).toBe(2.5);
+    formula.companies.forEach((c) => {
+      expect(c.priceEarnings >= 2.5).toBeTruthy();
+      expect(c.priceEarnings <= 4).toBeTruthy();
+    });
   });
 
   test("should apply return on assetfilter", () => {
     const data = [
-      { ticket: "eternit3", priceEarnings: 2.5, returnOnAsset: 20 },
-      { ticket: "tasa3", priceEarnings: 2, returnOnAsset: 30 },
-      { ticket: "tasa3", priceEarnings: 5, returnOnAsset: 40 },
+      new Company("eternit3", 2.5, 20),
+      new Company("tasa3", 2, 30),
+      new Company("tasa4", 5, 40),
     ];
     const formula = new MagicFormula(data);
 
     formula.filterReturnOnAsset(25, 50);
 
-    expect(formula.companies).toHaveLength(2);
     formula.companies.forEach((c) => {
       expect(c.returnOnAsset >= 25).toBeTruthy();
       expect(c.returnOnAsset <= 50).toBeTruthy();
